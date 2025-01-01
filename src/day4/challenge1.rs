07515c72ffe00f62b28d3536918ded8862203d55
amusgrove <alex_musgrove@intuit.com>
2025-01-01 20:47:05 +0000

    day 4 part 1 attempt 1

--- src/day4/challenge1.rs
+++ src/day4/challenge1.rs
@@ -1,8 +1,56 @@
 pub fn solve(input: Vec<Vec<char>>) -> i32 {
     let mut count = 0;
     
-    for line in input {
-        count += analyse_line(line);
+    for line in input.clone() { // check each line horizontally
+        count += analyse_line(line.clone());
+        
+        // check again in reverse
+        let mut reversed_line = line.clone();
+        reversed_line.reverse();
+        count += analyse_line(reversed_line);
+    }
+    
+    for i in 0..input[0].clone().len() { // check each line vertically
+        let mut line = Vec::new();
+        for j in 0..input.len() {
+            line.push(input[j][i]);
+        }
+        count += analyse_line(line.clone());
+        
+        // check again in reverse
+        let mut reversed_line = line.clone();
+        reversed_line.reverse();
+        count += analyse_line(reversed_line);
+    }
+    
+    for i in 0..input.len() { // check each line diagonally - top left to bottom right
+        let mut line = Vec::new();
+        for j in 0..input.len() {
+            if i + j < input.len() {
+                line.push(input[j][i + j]);
+            }
+        }
+        count += analyse_line(line.clone());
+        
+        // check again in reverse
+        let mut reversed_line = line.clone();
+        reversed_line.reverse();
+        count += analyse_line(reversed_line);
+    }
+    
+    for i in input.len() - 1..0 { // check each line diagonally - bottom left to top right
+        let mut line = Vec::new();
+        for j in 0..input.len() {
+            if i - j >= 0 {
+                line.push(input[j][i - j]);
+            }
+        }
+        count += analyse_line(line.clone());
+        
+        // check again in reverse
+        let mut reversed_line = line.clone();
+        reversed_line.reverse();
+        count += analyse_line(reversed_line);
     }
     
     count
@@ -11,51 +59,22 @@ pub fn solve(input: Vec<Vec<char>>) -> i32 {
 fn analyse_line(line: Vec<char>) -> i32 {
     let mut count = 0;
     let mut last_char = '.';
+
     for char in line.clone() {
-        if char == 'X' {
-            last_char = 'X';
-            continue;
-        }
-        
-        if last_char == 'X' {
-            if char == 'X' { 
-                continue;    
-            };
-            
-            if char == 'M' {
-                last_char = 'M';
-                continue;
-            };
-        }
-        if last_char == 'M' {
-            if char == 'M' {
-                continue;
-            }
-            
-            if char == 'A' {
-                last_char = 'A';
-                continue;
-            }
-        }
-        if last_char == 'A' {
-            if char == 'A' {
-                continue;
-            }
-            
-            if char == 'S' {
-                println!("Found XMAS! count: {}", line.clone().iter().collect::<String>());                
+        match (last_char, char) {
+            ('X', 'M') => last_char = 'M',
+            ('M', 'A') => last_char = 'A',
+            ('A', 'S') => {
                 count += 1;
                 last_char = 'S';
-                continue;
-            }
-        }
-        
-        if last_char == 'S' {
-            if char == 'S' {
-                continue;
-            }
+                println!("Found XMAS! count: {}", line.iter().collect::<String>());
+            },
+            ('X', 'X') | ('M', 'M') | ('A', 'A') | ('S', 'S') => {},
+            ('X', _) | ('M', _) | ('A', _) | ('S', _) => last_char = '.',
+            (_, 'X') => last_char = 'X',
+            _ => last_char = '.',
         }
     }
-    
+
     count
 }
