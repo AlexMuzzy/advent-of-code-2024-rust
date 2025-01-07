ee8b23b8ecc3e13f0d026f822b9fe8a479ae79b9
AlexMuzzy <alexmuzzy33@gmail.com>
2025-01-07 22:16:05 +0000

    day 4 progress

--- src/day4/challenge1.rs
+++ src/day4/challenge1.rs
@@ -1,61 +1,114 @@
 pub fn solve(input: Vec<Vec<char>>) -> i32 {
     let mut count = 0;
-    
-    for line in input {
-        count += analyse_line(line);
+
+    println!("Checking {}x{} grid", input.len(), input[0].len());
+
+    println!("Checking horizontally");
+    for mut line in input.clone() {
+        // check each line horizontally
+        count += analyse_line(&line);
+
+        // check again in reverse
+        line.reverse();
+        count += analyse_line(&line);
     }
-    
+
+    println!("Checking vertically");
+    for i in 0..input[0].clone().len() {
+        // check each line vertically
+        let mut line = Vec::new();
+        for j in 0..input.len() {
+            line.push(input[j][i]);
+        }
+        count += analyse_line(&line);
+
+        // check again in reverse
+        line.reverse();
+        count += analyse_line(&line);
+    }
+
+    println!("Checking diagonally - top left to bottom right");
+    // check each line diagonally - top left to bottom right
+    for i in 0..input.len() {
+        let mut column_line = Vec::new();
+        let mut row_line = Vec::new();
+        for j in 0..input.len() {
+            if i + j < input.len() {
+                println!("Adding {} to column_line, x={}, y={}", input[j][i + j], j, i + j);
+                column_line.push(input[j][i + j]);
+            }
+        }
+
+        for j in 1..input.len() {
+            if i + j < input.len() {
+                // skip the centre line as it's already been checked
+                row_line.push(input[i + j][j]);
+            }
+        }
+        count += analyse_line(&column_line);
+        count += analyse_line(&row_line);
+
+        // check again in reverse
+        column_line.reverse();
+        count += analyse_line(&column_line);
+
+        row_line.reverse();
+        count += analyse_line(&row_line);
+    }
+
+    println!("Checking diagonally - bottom left to top right");
+    for i in (0..input.len()).rev() {
+        let mut column_line = Vec::new();
+        let mut row_line = Vec::new();
+        for j in 0..input.len() {
+            let ii = i as i32;
+            let jj = j as i32;
+            if ii - jj >= 0 {
+                println!("Adding {} to column_line, x={}, y={}", input[i - j][j], i - j, j);
+                column_line.push(input[i - j][j]);
+            }
+        }
+
+        for j in 1..input.len() {
+            let ii = i as i32;
+            let jj = j as i32;
+            if ii - jj >= 0 {
+                // skip the centre line as it's already been checked
+                row_line.push(input[i - j][j]);
+            }
+        }
+
+        count += analyse_line(&column_line);
+        count += analyse_line(&row_line);
+
+        // check again in reverse
+        column_line.reverse();
+        count += analyse_line(&column_line);
+
+        row_line.reverse();
+        count += analyse_line(&row_line);
+    }
+
     count
 }
 
-fn analyse_line(line: Vec<char>) -> i32 {
+fn analyse_line(line: &Vec<char>) -> i32 {
+    let mut score = 0;
     let mut count = 0;
-    let mut last_char = '.';
-    for char in line.clone() {
-        if char == 'X' {
-            last_char = 'X';
-            continue;
-        }
-        
-        if last_char == 'X' {
-            if char == 'X' { 
-                continue;    
-            };
-            
-            if char == 'M' {
-                last_char = 'M';
-                continue;
-            };
-        }
-        if last_char == 'M' {
-            if char == 'M' {
-                continue;
-            }
-            
-            if char == 'A' {
-                last_char = 'A';
-                continue;
-            }
-        }
-        if last_char == 'A' {
-            if char == 'A' {
-                continue;
-            }
-            
-            if char == 'S' {
-                println!("Found XMAS! count: {}", line.clone().iter().collect::<String>());                
+
+    for char in line {
+        match (score, char) {
+            (0, 'X') => score = 1,
+            (1, 'M') => score = 2,
+            (2, 'A') => score = 3,
+            (3, 'S') => {
+                score = 0;
                 count += 1;
-                last_char = 'S';
-                continue;
+                println!("Found XMAS! count: {}", line.iter().collect::<String>());
             }
-        }
-        
-        if last_char == 'S' {
-            if char == 'S' {
-                continue;
-            }
+            (_, _) => score = 0, // This catches all other combinations
         }
     }
-    
+
     count
 }
